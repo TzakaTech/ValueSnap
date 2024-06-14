8337bdb46ef3171941d95c511cca789a304ebbf9
Dtzaka <63433619+TzakaTech@users.noreply.github.com>
2024-06-14 14:26:31 -0400

    Add files via upload

---
+++ valuesnap/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/denistzaka/CompSci/RPI/RCOS/ValueSnap/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/denistzaka/CompSci/RPI/RCOS/ValueSnap/valuesnap"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/denistzaka/CompSci/RPI/RCOS/ValueSnap/valuesnap/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9lZGQ4NTQ2MTE2NDU3YmRmMWM1YmRmYjEzZWNiOTQ2M2QyYmI1ZWQ0Lw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/denistzaka/CompSci/RPI/RCOS/ValueSnap/valuesnap/.dart_tool/package_config.json"
